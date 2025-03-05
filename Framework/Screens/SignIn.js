0b31b18cbef485d6ca2cc99bdab516b858a929f5
Anthony Godspower <anthonygodspowerstephen@gmail.com>
2025-03-05 01:15:53 +0100

    Push

--- Framework/Screens/SignIn.js
+++ Framework/Screens/SignIn.js
@@ -29,7 +29,7 @@ export function SignIn({ navigation }) {
                             .then((data) => {
                                 setUserUID(data.user.uid);
                                 setPreloader(false)
-                                navigation.navigate("Homescreen")
+                                navigation.reset({ index: 0, routes: [{ name: "Homescreen", }] })
                             })
                             .catch(e => {
                                 setPreloader(false)
